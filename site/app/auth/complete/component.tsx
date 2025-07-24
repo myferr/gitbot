6c93e17e563ab34cc26fbdfc57e596e994b1c560
myferr <contactme.myfer@protonmail.com>
2025-07-24 11:10:11 -0500

    fix deployment issue

---
+++ site/app/auth/complete/component.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function CompleteClient() {
+  const searchParams = useSearchParams();
+  const [discordId, setDiscordId] = useState("");
+
+  useEffect(() => {
+    const discord = searchParams.get("discord");
+    if (discord) setDiscordId(discord);
+  }, [searchParams]);
+
+  return (
+    <main className="flex flex-1 flex-col justify-center items-center min-h-screen bg-gray-900 text-white p-8">
+      <div className="max-w-xl w-full text-center space-y-4 rounded-lg bg-gray-800 p-10 shadow-lg">
+        <h1 className="text-4xl font-extrabold">
+          You're linked,&nbsp;
+          <span className="bg-gradient-to-r from-pink-500 via-yellow-500 to-blue-500 bg-clip-text text-transparent">
+            {discordId || "Unknown User"}
+          </span>
+          !
+        </h1>
+        <p className="text-gray-400 text-lg">You can now return to Discord.</p>
+      </div>
+    </main>
+  );
+}
+
